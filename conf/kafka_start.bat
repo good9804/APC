1c04c10195b2c19244fc0c51914e46e496b139bc
good9804 <ggoggoni98@gmail.com>
2023-09-05 16:17:12 +0900

    add

--- conf/kafka_start.bat
+++ conf/kafka_start.bat
@@ -1,11 +1,15 @@
 @echo off
 cd /d %~dp0
 REM the example of running batch files in sequence and in the background
-
+copy zookeeper.properties ..\kafka\config\
+copy server1.properties ..\kafka\config\
+copy server2.properties ..\kafka\config\
+copy server3.properties ..\kafka\config\
+copy nginx.conf ..\nginx\conf\
 start /B "" cmd /c "cd  ..\kafka&&bin\windows\zookeeper-server-start.bat config\zookeeper.properties"
 start /B "" cmd /c "bin\windows\kafka-server-start.bat config\server1.properties"
 start /B "" cmd /c "bin\windows\kafka-server-start.bat config\server2.properties"
 start /B "" cmd /c "bin\windows\kafka-server-start.bat config\server3.properties"
-start /B "" cmd /c "cd  ..\kafka&&nginx -s reload"
+start /B "" cmd /c "cd  ..\nginx&&nginx -s reload"
 
 pause
