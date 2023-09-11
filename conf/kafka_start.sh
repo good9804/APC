84ec6e59e4aaf126fe23f460b298ebbf4e8660bf
good9804 <ggoggoni98@gmail.com>
2023-09-12 01:33:22 +0900

    kafka 변경

--- conf/kafka_start.sh
+++ conf/kafka_start.sh
@@ -7,10 +7,11 @@ cp server2.properties ../kafka/config/
 cp server3.properties ../kafka/config/
 
 # 백그라운드에서 Zookeeper와 Kafka 서버를 시작
-nohup ../kafka/bin/zookeeper-server-start.sh ../kafka/config/zookeeper.properties > /dev/null 2>&1 &
-../kafka/bin/kafka-server-start.sh ../kafka/config/server1.properties > /dev/null 2>&1 &
-../kafka/bin/kafka-server-start.sh ../kafka/config/server2.properties > /dev/null 2>&1 &
-../kafka/bin/kafka-server-start.sh ../kafka/config/server3.properties > /dev/null 2>&1 &
+../kafka/bin/zookeeper-server-start.sh ../kafka/config/zookeeper.properties 2>&1 &
+sleep 5
+../kafka/bin/kafka-server-start.sh ../kafka/config/server1.properties 2>&1 &
+../kafka/bin/kafka-server-start.sh ../kafka/config/server2.properties 2>&1 &
+../kafka/bin/kafka-server-start.sh ../kafka/config/server3.properties 2>&1 &
 
 # 실행을 일시 중지
 read -p "Press Enter to exit..."
