f894bee01e823f8f996aadce1a460d2c54a9c636
good9804 <ggoggoni98@gmail.com>
2023-10-04 14:57:04 +0900

    bat 수정

--- conf/kafka_start.bat
+++ conf/kafka_start.bat
@@ -7,9 +7,9 @@ copy server2.properties ..\kafka\config\
 copy server3.properties ..\kafka\config\
 
 start /B "" cmd /c "cd  ..\kafka&&bin\windows\zookeeper-server-start.bat config\zookeeper.properties"
-start /B "" cmd /c "bin\windows\kafka-server-start.bat config\server1.properties"
-start /B "" cmd /c "bin\windows\kafka-server-start.bat config\server2.properties"
-start /B "" cmd /c "bin\windows\kafka-server-start.bat config\server3.properties"
+start /B "" cmd /c "cd  ..\kafka&&bin\windows\kafka-server-start.bat config\server1.properties"
+start /B "" cmd /c "cd  ..\kafka&&bin\windows\kafka-server-start.bat config\server2.properties"
+start /B "" cmd /c "cd  ..\kafka&&bin\windows\kafka-server-start.bat config\server3.properties"
 
 
 pause
